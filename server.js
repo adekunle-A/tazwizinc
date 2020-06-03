ab03198d99c0f8a0481220566d3f2b7a53795ef7
aaa90 <31294512+aaa90@users.noreply.github.com>
2020-06-02 21:56:53 -0400

    tazwizinc template

---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/build'))
+app.get('/', (req, res, next) => {
+    res.sendFile(__dirname + '/build/index.html')
+})
+app.listen(port, () => console.log(`Listening on port ${port}`));
