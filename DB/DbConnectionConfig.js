e22885dbb1d801fb281a604ebe0257400083385f
aaa90 <31294512+aaa90@users.noreply.github.com>
2020-06-06 03:25:15 -0400

    version 1.1.0

--- DB/DbConnectionConfig.js
+++ DB/DbConnectionConfig.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose") // calling the mongoose module
 
-const URL = process.env.MONGODB_URI || "mongodb+srv://dbAdminUser:ZIEN1QEHCl1lcCYf@cluster0-fz3nf.mongodb.net/tazwiz?retryWrites=true&w=majority" // DB connection URL
+const URL = process.env.MONGOLAB_URI || process.env.MONGOHQ_URL || "mongodb+srv://dbAdminUser:ZIEN1QEHCl1lcCYf@cluster0-fz3nf.mongodb.net/tazwiz?retryWrites=true&w=majority" // DB connection URL
 
 // function to async connec to the database
 const connectDB = async () => {
